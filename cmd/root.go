1ff990d111199264f7e6cd04f659b2e65e6218e7
moxcelix <maxim.spf@gmail.com>
2025-09-02 11:27:23 +0300

    address changed

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,7 @@ func Run() any {
 		route route.Routes,
 	) {
 		route.Setup()
-		err := handler.Gin.Run(env.ApiURL + ":" + env.Port)
+		err := handler.Gin.Run(":" + env.Port)
 
 		if err != nil {
 			logger.Error(err)
