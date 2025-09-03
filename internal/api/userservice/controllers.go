eff41c7909c2b99394ab4d301cfb8b37488345d9
moxcelix <maxim.spf@gmail.com>
2025-09-03 18:49:36 +0300

    add user service routes

--- internal/api/userservice/controllers.go
+++ internal/api/userservice/controllers.go
@@ -1,1 +1,48 @@
 package userservice_api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	userservice_application "main/internal/application/userservice"
+)
+
+type UserRegisterController struct {
+	registerUseCase *userservice_application.RegisterUseCase
+}
+
+func NewUserRegisterController(registerUseCase *userservice_application.RegisterUseCase) *UserRegisterController {
+	return &UserRegisterController{
+		registerUseCase: registerUseCase,
+	}
+}
+
+type registerRequest struct {
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Bio      string `json:"bio"`
+}
+
+type registerResponse struct {
+	Message string `json:"message"`
+}
+
+func (c *UserRegisterController) Register(ctx *gin.Context) {
+	var req registerRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := c.registerUseCase.Execute(req.Name, req.Username, req.Password, req.Bio)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, registerResponse{
+		Message: "user registered successfully",
+	})
+}
--- internal/api/userservice/routes.go
+++ internal/api/userservice/routes.go
@@ -1,1 +1,26 @@
 package userservice_api
+
+import (
+	"main/pkg"
+)
+
+type UserServiceRoutes struct {
+	handler                pkg.RequestHandler
+	userRegisterController *UserRegisterController
+}
+
+func NewUserServiceRoutes(
+	userRegisterRegister *UserRegisterController,
+	handler pkg.RequestHandler,
+) UserServiceRoutes {
+	return UserServiceRoutes{
+		userRegisterController: userRegisterRegister,
+		handler:                handler,
+	}
+}
+
+func (r UserServiceRoutes) Setup() {
+	group := r.handler.Gin.Group("/api/v1/user")
+
+	group.GET("/register", r.userRegisterController.Register)
+}
