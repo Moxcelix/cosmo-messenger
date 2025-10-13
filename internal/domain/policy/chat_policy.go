632857ac3df8b705d974ce02e1d26a528541c290
moxcelix <maxim.spf@gmail.com>
2025-10-13 12:15:00 +0300

    implement chat policy

--- internal/domain/policy/chat_policy.go
+++ internal/domain/policy/chat_policy.go
@@ -1,1 +1,41 @@
 package policy_domain
+
+import (
+	"errors"
+)
+
+var (
+	ErrShortChatName   = errors.New("chat name is to short")
+	ErrLongChatName    = errors.New("chat name is to long")
+	ErrManyChatMembers = errors.New("too many members in chat")
+)
+
+type ChatPolicy struct {
+	cfg ChatPolicyConfig
+}
+
+func NewChatPolicy(cfg ChatPolicyConfig) *ChatPolicy {
+	return &ChatPolicy{
+		cfg: cfg,
+	}
+}
+
+func (p *ChatPolicy) ValidateChatName(chatName string) error {
+	if len(chatName) < p.cfg.MinChatNameLength() {
+		return ErrShortChatName
+	}
+
+	if len(chatName) > p.cfg.MaxChatNameLength() {
+		return ErrLongChatName
+	}
+
+	return nil
+}
+
+func (p *ChatPolicy) ValidateGroupMembers(count int) error {
+	if count > p.cfg.MaxGroupMembers() {
+		return ErrManyChatMembers
+	}
+
+	return nil
+}
