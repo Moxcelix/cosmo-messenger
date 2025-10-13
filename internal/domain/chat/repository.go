4af0280e6ee33c2912843cc5a0c62986860c7b2b
moxcelix <maxim.spf@gmail.com>
2025-10-13 18:20:33 +0300

    add chat repo interface

---
+++ internal/domain/chat/repository.go
@@ -0,0 +1,9 @@
+package chat_domain
+
+type ChatRepository interface {
+	Create(chat *Chat) error
+	GetByID(id string) (*Chat, error)
+	GetByMember(userID string) ([]*Chat, error)
+	Update(chat *Chat) error
+	Delete(id string) error
+}
